1d7c2518f0937c3f9b73e8d6ce4d18fe9c82929e
Arnaldo-MarcAngelo-BSIT32E1 <145685247+Lapan-Ace-BSIT32E1@users.noreply.github.com>
2024-06-15 15:55:08 +0800

    Fixed

--- VideoStreaming/Video_Streaming/obj/Debug/net8.0/Video_Streaming.AssemblyInfo.cs
+++ VideoStreaming/Video_Streaming/obj/Debug/net8.0/Video_Streaming.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Video_Streaming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9ee434a30fdc5879f84c43bc9102ba4b4d75a44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8328b58f6a12bbe219796518a2a7a7762a76155")]
 [assembly: System.Reflection.AssemblyProductAttribute("Video_Streaming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Video_Streaming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
